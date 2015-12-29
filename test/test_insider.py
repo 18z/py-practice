0416c0cbdc4d2bfed78d3a16c47657d8a7601185
deanboole <xspiritualx@gmail.com>
2015-12-29 15:03:14 +0800

    no input manually while testing

--- test/test_insider.py
+++ test/test_insider.py
@@ -5,7 +5,7 @@ class TestInsiderFunction(unittest.TestCase):
 
     def test_ping(self):
         host = insider("redbull")
-        self.assertEqual("8.8.8.8 is Up", host.check_ip(raw_input("Enter IP: ")))
+        self.assertEqual("8.8.8.8 is Up", host.check_ip("8.8.8.8"))
 
     def test_firewall_log(self):
         host = insider("redbull")
