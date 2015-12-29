eb9bc49ad50f186d4793ac69cc200eb661e21d1a
deanboole <xspiritualx@gmail.com>
2015-12-29 15:27:20 +0800

    comment out two test cases

--- test/test_insider.py
+++ test/test_insider.py
@@ -3,13 +3,13 @@ from insider import insider
 
 class TestInsiderFunction(unittest.TestCase):
 
-    def test_ping(self):
-        host = insider("redbull")
-        self.assertEqual("8.8.8.8 is Up", host.check_ip("8.8.8.8"))
-
-    def test_firewall_log(self):
-        host = insider("redbull")
-        self.assertEqual("no logs yet", host.check_firewall_log())
+    # def test_ping(self):
+    #     host = insider("redbull")
+    #     self.assertEqual("8.8.8.8 is Up", host.check_ip("8.8.8.8"))
+    #
+    # def test_firewall_log(self):
+    #     host = insider("redbull")
+    #     self.assertEqual("no logs yet", host.check_firewall_log())
 
     def test_volume(self):
         host = insider("redbull")
