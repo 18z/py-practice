7c32c0a554c333c5983e8ef173ef1f11edda6012
deanboole <xspiritualx@gmail.com>
2015-12-30 15:52:59 +0800

    keep it simple

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 [![Build Status](https://travis-ci.org/deanboole/py-practice.svg)](https://travis-ci.org/deanboole/py-practice)
 [![Coverage Status](https://coveralls.io/repos/deanboole/py-practice/badge.svg?branch=master&service=github)](https://coveralls.io/github/deanboole/py-practice?branch=master)
-123
--- insider.py
+++ insider.py
@@ -13,39 +13,3 @@ class insider(object):
                 return "volume is enough use"
         except:
             print "see if df dies"
-
-    def check_ip(self, ip):
-        try:
-            received = subprocess.check_output("ping "+ ip + \
-                " -c 1 |grep -E -o '[0-9]+ received'| cut -f1 -d' '", shell = True)
-            if int(received) == 1:
-                response = ip + " is Up"
-            else:
-                response = ip + " is Down"
-            return response
-        except:
-            print "see if ping dies"
-
-    # def check_disk_health(self, hd):
-    #     try:
-    #         health = subprocess.check_output("sudo smartctl -q errorsonly -H -A -a " + \
-    #             hd, shell = True)
-    #         good = ""
-    #         if health == good:
-    #             health_st = "Good"
-    #         else:
-    #             helth_st = "Bad"
-    #         return health_st
-    #     except:
-    #         print "exit status = 1"
-
-    def check_firewall_log(self):
-        try:
-            log_num = subprocess.check_output("grep iptables /var/log/syslog|wc -l", shell = True)
-            if int(log_num) == 0:
-                log_status = "no logs yet"
-            else:
-                log_status = "logs exits"
-            return log_status
-        except:
-            print "check iptable log"
