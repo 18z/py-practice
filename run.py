080089b95dd11611cc314587badfb06fbf466563
deanboole <xspiritualx@gmail.com>
2016-01-25 15:44:42 +0800

    add argument

--- run.py
+++ run.py
@@ -8,9 +8,13 @@ host = insider("redbull")
 
 parser = argparse.ArgumentParser(description='check services.')
 parser.add_argument("-v", "--volume", help="print string")
+parser.add_argument("argument")
 args = parser.parse_args()
 
 if args.volume == "check":
     print host.check_volume()
+
+if args.argument == "magic":
+    print args.argument
 
 #print host.check_firewall_log()
