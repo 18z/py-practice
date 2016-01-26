e8216e829de36b939bdd0730c4755c11fd879b54
deanboole <xspiritualx@gmail.com>
2016-01-26 11:00:21 +0800

    add example of url parser

--- file_hash.py
+++ file_hash.py
@@ -1,6 +1,8 @@
 from lib.object import File
 
-f = File("insider.py")
+f = File("test.txt")
 
-print f.get_name()
-print f.get_sha512()
+#print f.get_apk_entry()
+#print f.get_name()
+#print f.get_sha512()
+print f.get_urls()
