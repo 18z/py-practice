993ee4b79937a310141eb631bda1327e7c1922fb
deanboole <xspiritualx@gmail.com>
2016-01-26 11:19:34 +0800

    add example of android apk entry point

--- file_hash.py
+++ file_hash.py
@@ -1,8 +1,8 @@
 from lib.object import File
 
-f = File("test.txt")
+f = File("2016-01-06_LIFESTYLE_1_com.paktor.apk")
 
-#print f.get_apk_entry()
+print f.get_apk_entry()
 #print f.get_name()
 #print f.get_sha512()
-print f.get_urls()
+#print f.get_urls()
